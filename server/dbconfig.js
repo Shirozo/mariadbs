f7b385c182432757d08609ae17f2d2b9d25d3335
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2023-08-19 23:01:15 +0800

    HEHEHEE

--- server/dbconfig.js
+++ server/dbconfig.js
@@ -1,8 +1,8 @@
 import express from "express"
-import mysql from 'mysql'
+import mariadb from 'mariadb'
 import cors from 'cors'
 
-const pool = mysql.createConnection({
+const pool = mariadb.createPool({
     host: '127.0.0.1',
     user: 'root',
     port: 3306,
@@ -12,14 +12,52 @@ const pool = mysql.createConnection({
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    pool.query("SELECT * FROM data", (err, result) => {
-        if (err) return res.json({"Message" : "Erorr"})
+app.get("/", async (req, res) => {
+    try {
+        const conn = await pool.getConnection()
+        const result = await conn.query("SELECT * FROM data ORDER BY name")
+        conn.end()
         return res.json(result)
-    })
+    }
+    catch (err) {
+        return res.json({"Message" : "An error occured"})
+    }
+    // pool.query("SELECT * FROM data ORDER BY name", (err, result) => {
+    //     if (err) return res.json({"Message" : "Erorr"})
+    //     return res.json(result)
+    // })
         
 })  
+
+app.post("/addstudent", async (req, res) => {
+    try {
+        const conn = await pool.getConnection()
+        const que = await conn.query(`INSERT INTO data (name, number, address, occupation)
+                                     VALUES(?, ?, ?, ?)`,
+                                     [
+                                        req.body.name,
+                                        req.body.number,
+                                        req.body.address,
+                                        req.body.occupation
+                                    ])
+        conn.commit()
+        conn.end()
+        return res.json(que)
+    }
+    catch (err) {
+        return res.json({"Message" : "An Error Occured"})
+    }
+
+    // pool.query(`INSERT INTO data (name, number, address, occupation) 
+    //             VAlUES(?, ?, ?, ?)`,
+    //             [data],
+    //             (err, result) => {
+    //                 if (err) return res.json({"Message": "An Error Occure"})
+    //                 return res.json(result)
+    //             })
+})
 
 app.listen(8081, () => {
     console.log("listening to 8081")

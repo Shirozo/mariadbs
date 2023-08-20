68e5e1984a8f61b6bc66b2c653b35e132c1443fa
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2023-08-20 17:27:30 +0800

    Mayda na dama hin comment

--- client/src/homepage.js
+++ client/src/homepage.js
@@ -4,6 +4,12 @@ import { Rendertbody } from "./JS/rendertbody"
 import { EditForm } from "./JS/editForm"
 
 export function HomePage() {
+    /*
+        HomePage or the Index of the page
+        It [newContact, setNewContact], [editingData, setEditingData], [editpress, setEditPress]
+        tas [userData, setUserData] is mga state hit im webpage. Kun mayda mag iba hit usa hit nga
+        state, magre-render lat it elements nga dependent hiton nga state dire tanan nga elements.
+     */
 
     const [newContact, setNewContact] = useState({
         name: "",
@@ -16,53 +22,76 @@ export function HomePage() {
     const [editpress, setEditPress] = useState(false)
     const [userData, setUserData] = useState([])
 
+    //useEffect nama is nagrarun everytime in nga DOM naglolooad. It ginrarun la niya is pagfetch hin data ha server
     useEffect(() => {
         getData()
     }, [])
 
     const getData = () => {
+        /*
+        Yanhi is pagfetch data ha express or server. Kulawa it ./server/dbconfig.js
+        tas it iya route nga "/". Nagfefetch la it hin data iton nga route 
+        */
         axios.get("http://localhost:8081/")
+        //setUserData is pag plug hit data ngadto hit state
         .then(res => setUserData([...res.data]))
         .catch(err => setUserData(null))
     }
 
     const addContact = (event) => {
+        /* 
+        Function pag add contact
+        preventDefault() para dire magrefresh it page kada submit
+        */
         event.preventDefault()
 
+        //Make an API request ngadto hit server gamit post para mahide it data nga parameter
         axios.post("http://localhost:8081/addcontact", newContact)
         .then(res => {
+            //Kun wara error ig reset it state
             setNewContact({
                 name: "",
                 number: "",
                 address: "",
                 occupation: "",
             })
+            //Update it state using getData() para magreflect it new data ha databse
             getData()
         })
+        //kun mayda error ig console.log lat error
         .catch(err => console.log(err))
     }
 
     const handleDelete = async (id) => {
+        /* 
+        function nama in pagtanggal data. notice wara didi preventDefault() kay clickable lat delete
+        ngan wara hiya didat form
+        */
         try {
+            //API request dadama ngadto ha server pluggin it id hit tatanggalon
             const res = await axios.delete("http://localhost:8081/removecontact/"+id)
+            //update dadama hit state
             getData()
         }
         catch (err) {
+            //log la dadama pag mayda error
             console.log(err)
         }
-
     }
 
     const handleEdit = async(id) => {
+        //Function para igpakita it modal nga edit
         const res = await axios.get("http://localhost:8081/user/"+id)
+        //update is editingData state han returned nga data from server
         setEditingData(...res.data)
+        //ipakita it modal
         setEditPress(true)
     }
-
 
     return(
         <>
             {editpress && (
+                //kun true it editpress nga state. ipakita it modal. kun dire, below element it makikita
                 <EditForm  
                     data={editingData} 
                     onUpdate={() => getData()}

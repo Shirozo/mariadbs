8e5a49c3bc8985cd609ef0d5ef1522882bf24f26
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2023-08-20 16:44:39 +0800

    Mayda na hin mga comments pakibasa nala

--- client/src/JS/editForm.js
+++ client/src/JS/editForm.js
@@ -6,10 +6,17 @@ export function EditForm({ data, onclose, onUpdate}) {
     const [newinfo, setnewinfo] = useState(data)
 
     const UpdateContact = async(event) => {
+        /* 
+        React function para mag edit/update hin data
+        */
+        //preventDefault() is para dire magreload it wesbite kun magsend hin form
         event.preventDefault()
         try{
+            //API call ngadto han express js passing an state nga newinfo
             const res = await axios.post("http://localhost:8081/update", newinfo)
+            //update it state
             onUpdate()
+            //close it modal
             onclose()
         }
         catch (err) {
@@ -19,6 +26,7 @@ export function EditForm({ data, onclose, onUpdate}) {
     }
 
     return (
+        /* Html hit pag edit hin data */
         <div className="editModal">
             <div className="editModalDiv">
                 <span className='close' onClick={onclose}>
@@ -32,6 +40,11 @@ export function EditForm({ data, onclose, onUpdate}) {
                         value={newinfo.name}
                         autoComplete='off'
                         autoFocus
+                        /*
+                        onChange is everytime magtype ka new letters ha input,
+                        mag a'update kon hain iya nga data hit state. Example is,
+                        name nga data hit state lat gin a'update hadi
+                        */
                         onChange={(newname) => setnewinfo((previnfo) => ({
                             ...previnfo, name: newname.target.value
                         }))}
@@ -42,6 +55,7 @@ export function EditForm({ data, onclose, onUpdate}) {
                         value={newinfo.number}
                         autoComplete='off'
                         autoFocus
+                        /* Pag update namat state hit data nga number */
                         onChange={(newnumber) => setnewinfo((previnfo) => ({
                             ...previnfo, number: newnumber.target.value
                         }))}
@@ -52,6 +66,7 @@ export function EditForm({ data, onclose, onUpdate}) {
                         value={newinfo.address}
                         autoComplete='off'
                         autoFocus
+                        /* Pag update namat state hit data nga address */
                         onChange={(newaddress) => setnewinfo((previnfo) => ({
                             ...previnfo, address: newaddress.target.value
                         }))}
@@ -62,6 +77,7 @@ export function EditForm({ data, onclose, onUpdate}) {
                         value={newinfo.occupation}
                         autoComplete='off'
                         autoFocus
+                        /* Pag update namat state hit data nga occupation */
                         onChange={(newoccupation) => setnewinfo((previnfo) => ({
                             ...previnfo, occupation: newoccupation.target.value
                         }))}
